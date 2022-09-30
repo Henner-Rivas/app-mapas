b626f388e2a7554a78fe0f438b1471bb589dd290
Henner-Rivas <89697511+Henner-Rivas@users.noreply.github.com>
2022-09-30 07:18:28 -0500

    creación de context y reducer de places

---
+++ src/MapApp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PlacesProvider } from "./context";
+
+const MapApp = () => {
+  return (
+    <PlacesProvider>
+      <h1>dd</h1>
+    </PlacesProvider>
+  );
+};
+
+export default MapApp;
---
+++ src/context/index.ts
@@ -0,0 +1,3 @@
+export { PlacesContext } from "./maps/PlacesContext";
+export { placesReducer } from "./maps/PlacesPeducer";
+export { PlacesProvider } from "./maps/PlacesProvider";
