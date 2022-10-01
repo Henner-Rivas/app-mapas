232fa8798e33293a4b4613388b8e13466927efa2
Henner-Rivas <89697511+Henner-Rivas@users.noreply.github.com>
2022-09-30 21:46:25 -0500

    buscador de sitos terminado

--- src/Components/SearchResult.tsx
+++ src/Components/SearchResult.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PlacesContext } from "../context/places/PlacesContext";
 import { LoadingPlaces } from "./LoadingPlaces";
+import { MapsContext } from "../context/maps/MapsContext";
+import { Feature } from "../interfaces/Places";
 
 export const SearchResult = () => {
   const { Places, isLoadingPlaces } = useContext(PlacesContext);
+  const { map } = useContext(MapsContext);
+  const [active, setActive] = useState("d");
 
   if (Places.length === 0) {
     return <></>;
@@ -12,13 +16,36 @@ export const SearchResult = () => {
   if (isLoadingPlaces) {
     return <LoadingPlaces />;
   }
+  const onPlacerClicked = (place: Feature) => {
+    const [lng, lat] = place.center;
+    map?.flyTo({
+      zoom: 13,
+      center: [lng, lat],
+    });
+    setActive(place.id);
+  };
+
   return (
     <ul className="list-group mt-3">
       {Places.map((place) => (
-        <li className="list-group-item list-group-item-action" key={place.id}>
+        <li
+          className={`list-group-item list-group-item-action pointer ${
+            active === place.id ? "active" : ""
+          }`}
+          key={place.id}
+          onClick={() => onPlacerClicked(place)}
+        >
           <h6>{place.text_es}</h6>
           <p>{place.place_name_es}</p>{" "}
-          <button className="btn btn-outline-primary">Direcciones</button>
+          <button
+            className={`btn ${
+              active === place.id
+                ? " btn-outline-light"
+                : " btn-outline-primary"
+            }`}
+          >
+            Direcciones
+          </button>
         </li>
       ))}
     </ul>
